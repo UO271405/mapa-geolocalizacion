4f3e68ee4d4fd1dfe3dc817bcca67f145533165c
UO271405 <60234300+UO271405@users.noreply.github.com>
2020-12-01 15:03:11 +0100

    Add files via upload

--- Geolocalizacion.js
+++ Geolocalizacion.js
@@ -1,16 +1,37 @@
 "use strict";
 class GeoLocalizacion{
     constructor (){
-        navigator.geolocation.getCurrentPosition(this.getPosition.bind(this));
+        navigator.geolocation.getCurrentPosition(this.getPosition.bind(this), this.verErrores.bind(this));
     }
     getPosition(position){
+        this.mensaje = "La petición de geolocalización se ha realizado con éxito.";
         this.longitud = position.coords.longitude; 
         this.latitud = position.coords.latitude;  
         this.precision = position.coords.accuracy;
+		
+		//Si descomento lo siguiente no me funciona (puede ser mi ordenador)
+		
         /*this.altitud          = posicion.coords.altitude;
         this.precisionAltitud = posicion.coords.altitudeAccuracy;
         this.rumbo            = posicion.coords.heading;
         this.velocidad        = posicion.coords.speed;*/
+    }
+
+    verErrores(error){
+        switch(error.code) {
+        case error.PERMISSION_DENIED:
+            this.mensaje = "El usuario no permite la petición de geolocalización"
+            break;
+        case error.POSITION_UNAVAILABLE:
+            this.mensaje = "Información de geolocalización no disponible"
+            break;
+        case error.TIMEOUT:
+            this.mensaje = "La petición de geolocalización ha caducado"
+            break;
+        case error.UNKNOWN_ERROR:
+            this.mensaje = "Se ha producido un error desconocido"
+            break;
+        }
     }
 
     show(){
@@ -27,6 +48,9 @@ class GeoLocalizacion{
         let liPrecision = document.createElement('li');
         liPrecision.append("Precisión de latitud y longitud: " + this.precision + " metros");
         coordenadas.appendChild(liPrecision);
+		
+		//Si descomento lo siguiente no me funciona (puede ser mi ordenador)
+		
         /*let liAltitud = document.createElement('li');
         liAltitud.append("Altitud: " + this.altitud + " metros");
         coordenadas.appendChild(liAltitud);
@@ -39,6 +63,37 @@ class GeoLocalizacion{
         let liVelocidad = document.createElement('li');
         liVelocidad.append("Velocidad: " + this.velocidad + " metros/segundo");
         coordenadas.appendChild(liVelocidad);*/
+    }
+
+    getMapaEstaticoGoogle(dondeVerlo){
+        var ubicacion=document.getElementById(dondeVerlo);
+        
+        var apiKey = "&key=AIzaSyC6j4mF6blrc4kZ54S6vYZ2_FpMY9VzyRU";
+        //URL: obligatoriamente https
+        var url = "https://maps.googleapis.com/maps/api/staticmap?";
+        //Parametros
+        // centro del mapa (obligatorio si no hay marcadores)
+        var centro = "center=" + this.latitud + "," + this.longitud;
+        //zoom (obligatorio si no hay marcadores)
+        //zoom: 1 (el mundo), 5 (continentes), 10 (ciudad), 15 (calles), 20 (edificios)
+        var zoom ="&zoom=15";
+        //Tamaño del mapa en pixeles (obligatorio)
+        var size = "&size=800x600";
+        //Escala (opcional)
+        //Formato (opcional): PNG,JPEG,GIF
+        //Tipo de mapa (opcional)
+        //Idioma (opcional)
+        //region (opcional)
+        //marcadores (opcional)
+        var marcador = "&markers=color:red%7Clabel:S%7C" + this.latitud + "," + this.longitud;
+        //rutas. path (opcional)
+        //visible (optional)
+        //style (opcional)
+        var sensor = "&sensor=false"; 
+        
+        this.imagenMapa = url + centro + zoom + size + marcador + sensor + apiKey;
+        
+        ubicacion.innerHTML = "<h2>Mapa de su localización</h2><img src='"+ this.imagenMapa +"' alt='Mapa'></img>";
     }
 }
 var posicion = new GeoLocalizacion();
