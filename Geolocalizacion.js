f781b3126122365883fb2ec787403c42de805f8e
UO271405 <60234300+UO271405@users.noreply.github.com>
2020-12-02 01:14:02 +0100

    Add files via upload

--- Geolocalizacion.js
+++ Geolocalizacion.js
@@ -1,16 +1,34 @@
 "use strict";
 class GeoLocalizacion{
     constructor (){
-        navigator.geolocation.getCurrentPosition(this.getPosition.bind(this));
+        navigator.geolocation.getCurrentPosition(this.getPosition.bind(this), this.verErrores.bind(this));
     }
     getPosition(position){
+        this.mensaje = "La petición de geolocalización se ha realizado con éxito";
         this.longitud = position.coords.longitude; 
         this.latitud = position.coords.latitude;  
         this.precision = position.coords.accuracy;
-        /*this.altitud          = posicion.coords.altitude;
+        this.altitud          = posicion.coords.altitude;
         this.precisionAltitud = posicion.coords.altitudeAccuracy;
         this.rumbo            = posicion.coords.heading;
-        this.velocidad        = posicion.coords.speed;*/
+        this.velocidad        = posicion.coords.speed;
+    }
+
+    verErrores(error){
+        switch(error.code) {
+        case error.PERMISSION_DENIED:
+            this.mensaje = "El usuario no permite la petición de geolocalización"
+            break;
+        case error.POSITION_UNAVAILABLE:
+            this.mensaje = "Información de geolocalización no disponible"
+            break;
+        case error.TIMEOUT:
+            this.mensaje = "La petición de geolocalización ha caducado"
+            break;
+        case error.UNKNOWN_ERROR:
+            this.mensaje = "Se ha producido un error desconocido"
+            break;
+        }
     }
 
     show(){
@@ -27,7 +45,7 @@ class GeoLocalizacion{
         let liPrecision = document.createElement('li');
         liPrecision.append("Precisión de latitud y longitud: " + this.precision + " metros");
         coordenadas.appendChild(liPrecision);
-        /*let liAltitud = document.createElement('li');
+        let liAltitud = document.createElement('li');
         liAltitud.append("Altitud: " + this.altitud + " metros");
         coordenadas.appendChild(liAltitud);
         let liPrecisionAltitud = document.createElement('li');
@@ -38,7 +56,7 @@ class GeoLocalizacion{
         coordenadas.appendChild(liRumbo);
         let liVelocidad = document.createElement('li');
         liVelocidad.append("Velocidad: " + this.velocidad + " metros/segundo");
-        coordenadas.appendChild(liVelocidad);*/
+        coordenadas.appendChild(liVelocidad);
     }
 }
 var posicion = new GeoLocalizacion();
